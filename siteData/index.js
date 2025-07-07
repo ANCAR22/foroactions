95c346e45051111485eab58ece30d1622663c4aa
ANCAR22 <carflores07a@gmail.com>
2025-07-06 18:31:42 -0600

    Primer commit

---
+++ siteData/index.js
@@ -0,0 +1,23 @@
+//import indexData from './pages/home';
+//import galeriaData from './pages/galeria';\
+
+function context(page){
+    console.log("Getting context for page:", page);
+    let context = {
+        title: "Mi sitio ABC Web",
+    }
+    switch(page){
+        /*case "/index.html":
+            context = {...context, ...cardsData, ...galeriaData, ...tablaData};
+            
+            break;
+        case "/galeria.html":
+            context = {...context, ...galeriaData};
+            break;*/
+    }
+    console.log("Context: ", JSON.stringify(context, null, 2));
+    return context;
+}
+
+
+export default context;
